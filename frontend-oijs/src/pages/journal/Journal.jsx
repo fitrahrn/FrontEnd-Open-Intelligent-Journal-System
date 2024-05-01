7105ad9ee510caefbad784289f3351c3bcfdf67a
Fitrah Ramadhani Nugroho <fitnug30@gmail.com>
2024-05-01 23:23:28 +0700

    Update Journal.jsx

--- frontend-oijs/src/pages/journal/Journal.jsx
+++ frontend-oijs/src/pages/journal/Journal.jsx
@@ -1,32 +1,18 @@
 import React,{ useState, useEffect }  from 'react';
 import LayoutJournal from '../../components/LayoutJournal';
 import { Link,useParams  } from "react-router-dom";
+import axios from "axios";
 const Journal = () => {
-    const [listArticle,setArticle] = useState([
-        {
-            id: 1,
-            title: "Improving the Performance of Low-resourced Speaker Identification with Data Preprocessing",
-            writer: "Articlei Lai Phyu, Hay Mar Soe Naing, Win Pa Pa",
-            year: 2023,
-            volume: 13,
-            issue: 3,
-            doi: "https://doi.org/10.5614/itbj.ict.res.appl.2023.17.3.1",
-
-        },
-        {
-            id: 2,
-            title: "An Efficient Intrusion Detection System to Combat Cyber Threats using a Deep Neural Network Model ",
-            writer: "Mangayarkarasi Ramaiah, C. Vanmathi, Mohammad Zubair Khan, M. Vanitha, M. Deepa ",
-            year: 2022,
-            volume: 11,
-            issue: 1,
-            doi: "https://doi.org/10.5614/itbj.ict.res.appl.2023.17.3.2 ",
-
-        }
-
-        
-    ]);
-    console.log(listArticle);
+    const [listArticle,setArticle] = useState([]);
+    const {journal} = useParams();
+    useEffect(() => {
+        getArticle();
+      }, []);
+    const getArticle = async () => {
+        const response = await axios.get(`http://localhost:3001/journal/${journal}/article`)
+        setArticle(response.data);
+    };
+    console.log(listArticle)
     return (
         <LayoutJournal>
                 <div id="content">
