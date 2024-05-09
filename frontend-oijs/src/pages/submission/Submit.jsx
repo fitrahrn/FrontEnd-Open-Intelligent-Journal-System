401847d0fbab983f215d2e200278a763bea526f8
Fitrah Ramadhani Nugroho <fitnug30@gmail.com>
2024-05-09 23:07:17 +0700

    connect submit article with backend

--- frontend-oijs/src/pages/submission/Submit.jsx
+++ frontend-oijs/src/pages/submission/Submit.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate,Link,useParams } from "react-router-dom";
+import axios from "axios";
 import LayoutSubmission from '../../components/LayoutSubmission';
 const Submit = () => {
+    const {journal} = useParams();
+    const [msg, setMsg] = useState("");
+    const [prefix, setPrefix] = useState("");
+    const [title, setTitle] = useState("");
+    const [subtitle, setSubtitle] = useState("");
+    const [abstract, setAbstract] = useState("");
+    const [keywords, setKeywords] = useState("");
+    const [contributors, setContributors] = useState("");
+    const [articlePath, setArticlePath] = useState("");
+    const navigate = useNavigate();
+
+    const Submit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post(`http://localhost:3001/article/${journal}`, {
+                prefix: prefix,
+                title:title,
+                subtitle:subtitle,
+                abstract:abstract,
+                keywords:keywords,
+                article_path:articlePath
+            });
+            navigate("/login");
+        } catch (error) {
+            if (error.response) {
+                setMsg(error.response.data.msg);
+            }
+        }
+    }
     return (
         <LayoutSubmission>
             <div className="App">
             <article class="card">
-                <div className="content-container">
+                <form className="content-container" onSubmit={Submit}>
+                    <p className="error" style={{color: "red"}}>{msg}</p>
                     <h1>Submit an Article</h1>
                     <h2>1. Section Policy</h2>
                     <h3>Submission Requirement</h3>
@@ -53,51 +85,51 @@ const Submit = () => {
                         </div>
                     </form>
                     <h2>3. Enter Meteadata</h2>
-                    <form class="result">
+                    <div class="result">
                         <div className="field">
                             <label className="label">Prefix</label>
                                 <div className="control">
-                                    <input type="text" className="input" placeholder='The'/>
+                                    <input type="text" className="input" placeholder='Prefix' value={prefix} onChange={(e) => setPrefix(e.target.value)}/>
                                 </div>
                         </div>
                         <div className="field">
                             <label className="label">Title</label>
                                 <div className="control">
-                                    <input type="text" className="input" placeholder='The'/>
+                                    <input type="text" className="input" placeholder='Title'value={title} onChange={(e) => setTitle(e.target.value)}/>
                                 </div>
                         </div>
                         <div className="field">
                             <label className="label">Subtitle</label>
                                 <div className="control">
-                                    <input type="text" className="input" placeholder='The'/>
+                                    <input type="text" className="input" placeholder='Subtitle' value={subtitle} onChange={(e) => setSubtitle(e.target.value)}/>
                                 </div>
                         </div>
                         <div className="field">
                             <label className="label">Abstract</label>
                                 <div className="control">
-                                    <input type="text" className="input" placeholder='The'/>
+                                    <input type="text" className="input" placeholder='Abstract' value={abstract} onChange={(e) => setAbstract(e.target.value)}/>
                                 </div>
                         </div>
                         <div className="field">
                             <label className="label">List Of Contributors</label>
                                 <div className="control">
-                                    <input type="text" className="input" placeholder='The'/>
+                                    <input type="text" className="input" placeholder='Contributors' value={contributors} onChange={(e) => setContributors(e.target.value)}/>
                                 </div>
                         </div>
                         <div className="field">
                             <label className="label">Additional Keywords</label>
                                 <div className="control">
-                                    <input type="text" className="input" placeholder='The'/>
+                                    <input type="text" className="input" placeholder='Keywords' value={keywords} onChange={(e) => setKeywords(e.target.value)}/>
                                 </div>
                         </div>
-                    </form>
+                    </div>
                     <h2>4. Confirmation</h2>
-                    <div class="submit">
-                        <button>Finish Submission</button>
+                    <div class="submit" >
+                        <button type="submit">Finish Submission</button>
                         <button class="cancel">Cancel</button>
                     </div>
                     
-                </div>
+                </form>
             </article>
                 
             </div>
