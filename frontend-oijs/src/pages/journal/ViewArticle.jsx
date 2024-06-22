ad22c76d2ad20080519b9c75e02f81be974cfc20
Fitrah Ramadhani Nugroho <fitnug30@gmail.com>
2024-06-22 23:03:13 +0700

    add article page

---
+++ frontend-oijs/src/pages/journal/ViewArticle.jsx
@@ -0,0 +1,57 @@
+import React,{ useState, useEffect }  from 'react';
+import LayoutJournal from '../../components/LayoutJournal';
+import { Link,useParams  } from "react-router-dom";
+import axios from "axios";
+import Layout from '../../components/Layout';
+const ViewArticle = () => {
+    const [article,setArticle] = useState([]);
+    const [authors,setAuthors] = useState([])
+    const {journal,article_id} = useParams();
+    useEffect(() => {
+        getArticle();
+      }, []);
+    const getArticle = async () => {
+        const response = await axios.get(`http://localhost:3001/article/${article_id}`)
+        setArticle(response.data);
+        setAuthors(response.data.authors)
+        console.log(response.data)
+    };
+    return (
+        <Layout>
+        <div className="container-fluid">
+            <div class="card w-75 m-3 p-5 mx-auto">
+                <div class="card-body ">
+                <h3 class="card-title fw-bold mb-5"> {article.title}</h3>
+                <p className='text-secondary'>Year Published: {article.year}</p>
+                    {authors.map((author) => (
+                        <div className="mb-2"> 
+                            <div class="fw-bold">{author.name}</div>
+                            <p class="card-text">{author.affiliation}, {author.country}</p>
+                        </div>
+                    ))}
+                    <p className='fw-bold mt-4 mb-4'>DOI: <Link className="fw-light" to={article.article_path}>{article.article_path}</Link></p>
+                    <p className='fw-bold mb-4'>Keywords: <span className='fw-normal'>{article.keywords}</span></p>
+                    <p className='fw-bold mb-4'>Cite :    
+                        <span className='fw-normal'> 
+                            {authors.map((author)=>{
+                                let array_name=author.name.split(" ");
+                                let first_name=array_name[0];
+                                return (<span>{array_name[array_name.length-1]}, {first_name[0]}., </span>)
+                            })}
+                            ({article.year}), "{article.title}", {article.journal_title}, {article.volume}({article.issue}), doi: {article.article_path}
+                        </span>
+                    </p>
+                    <p class="fw-bold">Abstract</p>
+                    <p class="card-text">{article.abstract}</p>
+                    <p class="card-text" >Issue: <Link to={`${journal}/issue/${article.volume}/${article.issue}`} class="card-text mb-3" >Vol. {article.volume} No. {article.issue} ({article.year})</Link> </p> 
+                    
+                    <Link to={`view`} class="btn btn-sm btn-outline-primary col-1">PDF</Link>
+                </div>
+                    
+            </div>
+        </div>
+        </Layout>
+    );
+}
+
+export default ViewArticle;
---
+++ frontend-oijs/src/pages/journal/ViewPDF.jsx
@@ -0,0 +1,56 @@
+import React,{ useState, useEffect }  from 'react';
+import LayoutJournal from '../../components/LayoutJournal';
+import { Link,useParams  } from "react-router-dom";
+import axios from "axios";
+import { Document, Page } from 'react-pdf';
+import Layout from '../../components/Layout';
+import { pdfjs } from 'react-pdf';
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css"
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+const ViewPDF = () => {
+    const [article,setArticle] = useState([]);
+    const [numPages, setNumPages] = useState();
+    const [pageNumber, setPageNumber] = useState(1);
+    const {journal,article_id} = useParams();
+    useEffect(() => {
+        getArticle();
+      }, []);
+    const getArticle = async () => {
+        const response = await axios.get(`http://localhost:3001/article/${article_id}`)
+        setArticle(response.data);
+    };
+    const nextPages = () =>{
+        if(numPages>pageNumber)setPageNumber(pageNumber+1)
+    }
+    const previousPages = () =>{
+        if(pageNumber>1)setPageNumber(pageNumber-1)
+    }
+
+    const onDocumentLoadSuccess= ({ numPages})=> {
+        setNumPages(numPages);
+    };
+    return (
+        <Layout>
+            <div class="card w-50 m-auto mt-3 p-4">
+                <div class="card-body">
+                    <Document className="ms-5" file={article.article_path} onLoadSuccess={onDocumentLoadSuccess}>
+                        <Page pageNumber={pageNumber} />
+                        <div className='row align-items-center'>
+                            <button className='col btn btn-primary' onClick={()=>previousPages()} >Previous Pages</button>
+                            <p className='col ms-5'>Page {pageNumber} of {numPages}</p>
+                            <button className='col btn btn-primary' onClick={()=>nextPages()} >Next Pages</button>
+                        </div>
+                    </Document>
+                    
+                </div>
+                
+            </div>
+        </Layout>
+    );
+}
+
+export default ViewPDF;
